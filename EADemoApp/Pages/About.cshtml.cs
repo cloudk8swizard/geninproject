45467b483fb81a230f8495c2753297b42b45f7c5
Marco Prado <maprado@microsoft.com>
2019-08-29 23:17:15 +0000

    Updated About.cshtml.cs with additional message description

--- EADemoApp/Pages/About.cshtml.cs
+++ EADemoApp/Pages/About.cshtml.cs
@@ -12,7 +12,7 @@ namespace EADemoApp.Pages
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = "Your application description page for the Google Auth.";
         }
     }
 }
